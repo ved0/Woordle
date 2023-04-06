1d82c96705596b5925beb5ae548e4dafbf4fdb4e
Vedad <vedadz@gmail.com>
2023-04-06 11:37:57 +0200

    Update Row.js
    * removed unused function, solved it through css instead

--- public/components/Row.js
+++ public/components/Row.js
@@ -1,11 +1,7 @@
-const toInputUppercase = e => {
-    e.target.value = ("" + e.target.value).toUpperCase();
-  };
-
 function Row() {
     let row = []
     for (let i = 0; i < n; ++i) {
-      row.push(<input type="text" minlength="1" maxlength="1" onInput={toInputUppercase} key={i}></input>)
+      row.push(<input type="text" minlength="1" maxlength="1" key={i}></input>)
     }
   
     return (
